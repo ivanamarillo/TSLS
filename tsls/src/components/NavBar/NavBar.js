156c94378a0807e3d9e3df0bfda6dfee1ae0aa5f
Ivan Amarillo <ivanamarillo88@gmail.com>
2022-05-09 21:11:14 -0500

    NavBar

---
+++ tsls/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+
+
+const NavBar = _ => {
+    return (
+        <nav className=''>
+            <div className=''>
+                <button>Celulares</button>
+                <button>Tablets</button>
+                <button>Notebook</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar();

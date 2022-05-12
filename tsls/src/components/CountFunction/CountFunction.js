3cb361ae524c32b69c102a009575b2f81b15f366
Ivan Amarillo <ivanamarillo88@gmail.com>
2022-05-11 19:42:16 -0500

    useState Hook

--- tsls/src/components/CountFunction/CountFunction.js
+++ tsls/src/components/CountFunction/CountFunction.js
@@ -1,15 +1,28 @@
+//ganchos
+import {useState} from 'react';
+
 const CountFunction = (props) => {
-    let count = 0;
+
+    // const state = useState(0);
+    // console.log(state);      //[0, func]
+    // const count = state[0];
+    // const setCount = state[1];
+
+    const [count, setCount] = useState(0);
+
     const decrement = _ => {
-        count--;
+        setCount(count - 1);
     };
+
     const increment = _ => {
-        count++;
-    };
+        setCount(count + 1)
+    }
+    
 
     console.log(props);
     return (
         <div>
+            <h1 style={{color:props.color}}>Contador Funcion</h1>
             <button onClick={decrement}>-</button>
             <h1 style={{color:props.color}}>{count}</h1>
             <button onClick={increment}>+</button>

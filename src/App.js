ea65ae82a757ced3a3fff3bd529bc8ceb041efaa
Ivan Amarillo <ivanamarillo88@gmail.com>
2022-05-23 19:55:45 -0500

    Fecth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import CountFunction from './components/CountFunction/CountFunction';
 import CountClass from './components/CountClass/CountClass';
+import MercadoLibre from './components/MercadoLibre/MercadoLibre';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBar/>
       <CountFunction color='blue'/>
       <CountClass color='red'/>
+      <MercadoLibre/>
     </div>
   );
 }
---
+++ src/components/MercadoLibre/MercadoLibre.js
@@ -0,0 +1,14 @@
+import {useEffect} from 'react';
+
+const MercadoLibre = () => {
+    useEffect(() => {
+        
+    }, [])
+    return (
+        <div>
+            <h1>Mercado Libre</h1>
+        </div>
+    )
+}
+
+export default MercadoLibre;

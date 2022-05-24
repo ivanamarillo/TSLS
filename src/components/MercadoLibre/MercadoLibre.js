60d5ff78a35969dedb3ffea21d84a0ee84e08103
Ivan Amarillo <ivanamarillo88@gmail.com>
2022-05-23 20:38:47 -0500

    Clase APIs

--- src/components/MercadoLibre/MercadoLibre.js
+++ src/components/MercadoLibre/MercadoLibre.js
@@ -1,12 +1,52 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 const MercadoLibre = () => {
-    useEffect(() => {
-        
-    }, [])
+    const [products, setProducts] = useState([]);
+    const [input, setInput] = useState('');
+    
+    //con handleSearch ya no se necesitaría
+    // useEffect(() => {
+    //     fetch(`https://api.mercadolibre.com/sites/MLA/search/?q=${input}`)
+    //     .then(res => {
+    //         return res.json()
+    //     })
+    //     .then(json => {
+    //         console.log(json);
+    //         setProducts(json.results);
+    //     })
+    // }, [])
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        fetch(`https://api.mercadolibre.com/sites/MLA/search/?q=${input}`)
+        .then(res => {
+            return res.json()
+        })
+        .then(json => {
+            console.log(json);
+            setProducts(json.results);
+        })
+
+    }
+
     return (
         <div>
             <h1>Mercado Libre</h1>
+            <form onSubmit={handleSearch}>
+                <input type='text' onChange={(e) => setInput(e.target.value)}/>
+                <button type='submit'>Buscar</button>
+            </form>
+            <ul>
+                {products.map(p => {
+                    return (
+                        <li key={p.id}>
+                            <img src={p.thumbnail} alt={p.title}/>
+                            <p>{p.title}</p>
+                            <p>{p.price}</p>
+                        </li>
+                    ) 
+                })}
+            </ul>
         </div>
     )
 }

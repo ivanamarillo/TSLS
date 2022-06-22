6c4947e76c90735d0b6c8be4d804bddda00b1b7f
Ivan Amarillo <ivanamarillo88@gmail.com>
2022-06-21 21:19:33 -0500

    Clase_1 repaso terminado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+/*--------------------------------
+Este es el concepto de componente:
 
+AvatarJSX es un componente que
+estoy insertando en el componente
+App.
+
+Puedo utilizarlo cuantas veces
+quiera cambiando sus parámetros
+por 'props'-
+--------------------------------*/
+
+const AvatarJSX = (props) => {
+  const src = `https://randomuser.me/api/portraits/lego/${props.id}.jpg`;
+  return (
+    <picture>
+      <img src={src} alt={'Lego'}/>
+      {props.name}
+    </picture>
+  );
+}
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <AvatarJSX id={1} name={'Ivan'}/>
+      <AvatarJSX id={2} name={'Seba'}/>
+      <AvatarJSX id={3} name={'Nico'}/>
+    </div>
   );
 }
 
